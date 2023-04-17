3925ce10a9b3b2b5a613f6ea0dea3daf20d7e335
Loa212 <anobileloris@gmail.com>
2023-04-17 15:31:29 +0200

    clean project

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,4 @@
+// @ts-ignore
 import Close from "./Close.astro";
-import CheckCircle from "./CheckCircle.astro";
-import LogoGithub from "./LogoGithub.astro";
 
-export { Close, CheckCircle, LogoGithub };
+export { Close };
