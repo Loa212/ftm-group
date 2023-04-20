7e7718a7d56a8711bd05eb5bb92b1f591da82b32
Loa212 <anobileloris@gmail.com>
2023-04-20 18:40:03 +0200

    add hero section and header

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,8 @@
 // @ts-ignore
 import Close from "./Close.astro";
+// @ts-ignore
+import GB from "./GB.astro";
+// @ts-ignore
+import IT from "./IT.astro";
 
-export { Close };
+export { Close, GB, IT };
